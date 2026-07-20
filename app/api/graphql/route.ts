12850982e95fc3d57dcf1a796325c92af7f8c9d6
Onifade Joshua Pamilerin <onifadjosh@gmail.com>
2026-07-20 16:03:11 +0100

    graphql

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,15 +1,25 @@
+import { dbConnect } from "@/app/libs/dbconnect";
+import UserModel from "@/app/models/User.model";
 import { gql } from "@apollo/client";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
-const typeDefs = gql`
+const typeDefs =gql`
   type User {
     name: String
     age: Int!
   }
+
+  type DBUser{
+     firstname:String
+    lastname:String
+    email:String
+  }
+
   type Query {
     hello: String
     person: User
+    userdb:[DBUser!]!
   }
 `;
 
@@ -19,9 +29,19 @@ const resolvers = {
     person: () => {
       return {
         name: "Onifade Joshua",
-        age:12
+        age: 12,
       };
     },
+
+    userdb:async()=>{
+      await dbConnect()
+
+      const users = await UserModel.find()
+      console.log(users);
+      
+
+      return users
+    }
   },
 };
 
